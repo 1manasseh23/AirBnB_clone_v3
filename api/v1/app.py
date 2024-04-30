74d28a0c6c37e9c5a61e7c98f56a5dbaae1f229e
1manasseh23 <cletavivan@gmail.com>
2024-04-29 23:28:26 -0700

    Xcoder

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,10 +6,12 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 from models import storage
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.teardown_appcontext
 def close_db(exception):
